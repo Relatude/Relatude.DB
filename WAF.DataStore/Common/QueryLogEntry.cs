73f9163eb5c40ccb57dd94c877d281ca91936bbb
Ole Gulbrandsen <o.gulbrandsen@gmail.com>
2025-08-25 16:41:10 +0200

    Delete QueryLogEntry.cs

--- WAF.DataStore/Common/QueryLogEntry.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WAF.Common {
-    public struct QueryLogEntry() {
-        public DateTime Timestamp { get; set; }
-        public string Query { get; set; }
-        public double Duration { get; set; }
-        public int Count { get; set; }
-    }
-}
