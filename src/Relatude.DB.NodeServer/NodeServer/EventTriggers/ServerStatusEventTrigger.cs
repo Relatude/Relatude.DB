0788fd3991384b681e2b054714c2262d274a9ead
Ole Gulbrandsen <o.gulbrandsen@gmail.com>
2025-10-02 15:21:52 +0200

    minor

--- src/Relatude.DB.NodeServer/NodeServer/EventTriggers/ServerStatusEventTrigger.cs
+++ src/Relatude.DB.NodeServer/NodeServer/EventTriggers/ServerStatusEventTrigger.cs
@@ -1,4 +1,5 @@
-﻿using Relatude.DB.Common;
+﻿using Microsoft.AspNetCore.Hosting.Server;
+using Relatude.DB.Common;
 
 namespace Relatude.DB.NodeServer.EventTriggers;
 
@@ -6,14 +7,16 @@ public class ServerStatusEventTrigger : IDisposable {
     RelatudeDBServer _server;
     Timer _pulseTimer;
     Dictionary<Guid, DataStoreStatus> _lastStatuses = [];
-    public ServerStatusEventTrigger() {
+    public ServerStatusEventTrigger( RelatudeDBServer server) {
+        _server = server;
         _pulseTimer = new Timer(_ => pulse(), null, 1000, 1000);
     }
     void pulse() {
         
     }
     bool updateIfChanged() {
-        
+        _server.GetC
+        return false;
     }
     public void Dispose() {
         _pulseTimer?.Dispose();
