4f600df0fc44e935024bb6daba7616b87a468c33
Marine Løken <marineloken@proton.me>
2025-10-17 16:52:43 +0200

    Add parameter extraction and improve expression evaluation in query builders

--- src/Relatude.DB.NodeStore/Query/ExpressionVisitor.cs
+++ src/Relatude.DB.NodeStore/Query/ExpressionVisitor.cs
@@ -1,21 +1,61 @@
 ﻿using Relatude.DB.Query.ExpressionToString.ExpressionTreeToString;
 using System.Linq.Expressions;
 using System.Reflection;
+
 namespace Relatude.DB.Query;
-internal static class ExpressionExtension {
-    public static string ToQueryString(this Expression expression) {
-        return new Visitor().Visit(expression).ToString("C#");
+
+internal static class ExpressionExtension
+{
+    public static string ToQueryString(this Expression expression, int parametersCount, out IReadOnlyCollection<Parameter> parameters)
+    {
+        var visited = new Visitor().Visit(expression);
+        parameters = visited.ExtractParameters();
+        return visited.ToString("C#");
     }
-    private class Visitor : ExpressionVisitor {
+
+    private class Visitor : ExpressionVisitor
+    {
         // making sure all local variables references are converted to actual values where possible
-        protected override Expression VisitMember(MemberExpression node) {
-            if (node.Member is FieldInfo fieldInfo && node.Expression is ConstantExpression constEx) {
-                var value = fieldInfo.GetValue(constEx.Value);
-                //if(value is Guid g) 
-                //    return Expression.Constant(g.ToString());
-                return Expression.Constant(value);
+        protected override Expression VisitMember(MemberExpression node)
+        {
+            // Try to evaluate member expressions that are based on constants or nested members of constants
+            if (!CanBeEvaluated(node))
+                return base.VisitMember(node);
+
+            var value = GetValue(node);
+            return Expression.Constant(value, node.Type);
+        }
+
+        private static bool CanBeEvaluated(Expression expr)
+        {
+            return expr switch
+            {
+                // Constants and member access of constants can be evaluated
+                ConstantExpression => true,
+                MemberExpression { Expression: not null } me => CanBeEvaluated(me.Expression),
+                _ => false
+            };
+        }
+
+        private static object? GetValue(Expression expr)
+        {
+            switch (expr)
+            {
+                case ConstantExpression c:
+                    return c.Value;
+
+                case MemberExpression { Expression: not null } m:
+                    var target = GetValue(m.Expression);
+                    return m.Member switch
+                    {
+                        FieldInfo fi => fi.GetValue(target),
+                        PropertyInfo pi => pi.GetValue(target),
+                        _ => throw new NotSupportedException($"Unsupported member: {m.Member.GetType().Name}")
+                    };
+
+                default:
+                    throw new NotSupportedException($"Cannot evaluate expression of type {expr.GetType().Name}");
             }
-            return base.VisitMember(node);
         }
     }
 }
