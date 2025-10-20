25eaa182f3df24001f0d881f88e87ecb552e4f1e
Marine Løken <marineloken@proton.me>
2025-10-20 15:05:03 +0200

    Refactor expression visitor to improve evaluation handling and enhance support for method calls, lambdas, and binary/unary operations.

--- src/Relatude.DB.NodeStore/Query/ExpressionVisitor.cs
+++ src/Relatude.DB.NodeStore/Query/ExpressionVisitor.cs
@@ -4,20 +4,30 @@ using System.Reflection;
 
 namespace Relatude.DB.Query;
 
-internal static class ExpressionExtension
+internal static class ExpressionExtensions
 {
     public static string ToQueryString(this Expression expression)
     {
-        var visited = new Visitor().Visit(expression);
+        var visited = new SubtreeEvaluator().Visit(expression);
         return visited.ToString("C#");
     }
 
-    private class Visitor : ExpressionVisitor
+    private class SubtreeEvaluator : ExpressionVisitor
     {
-        // making sure all local variables references are converted to actual values where possible
+        private readonly HashSet<ParameterExpression> _parameters = new();
+
+        protected override Expression VisitLambda<T>(Expression<T> node)
+        {
+            // collect lambda parameters so we don't evaluate expressions depending on them
+            foreach (var param in node.Parameters)
+                _parameters.Add(param);
+
+            var body = Visit(node.Body);
+            return Expression.Lambda(body, node.Parameters);
+        }
+
         protected override Expression VisitMember(MemberExpression node)
         {
-            // Try to evaluate member expressions that are based on constants or nested members of constants
             if (!CanBeEvaluated(node))
                 return base.VisitMember(node);
 
@@ -25,16 +35,39 @@ internal static class ExpressionExtension
             return Expression.Constant(value, node.Type);
         }
 
-        private static bool CanBeEvaluated(Expression expr)
+        protected override Expression VisitMethodCall(MethodCallExpression node)
         {
-            return expr switch
+            if (!CanBeEvaluated(node))
+                return base.VisitMethodCall(node);
+
+            var value = GetValue(node);
+            return Expression.Constant(value, node.Type);
+        }
+
+        private bool CanBeEvaluated(Expression? expr) =>
+            expr switch
             {
-                // Constants and member access of constants can be evaluated
+                // Anything that depends on parameters should *not* be evaluated
+                null => true,
+                ParameterExpression p when _parameters.Contains(p) => false,
                 ConstantExpression => true,
-                MemberExpression { Expression: not null } me => CanBeEvaluated(me.Expression),
+
+                // static member or member of evaluatable subtree
+                MemberExpression me => (me.Expression == null || CanBeEvaluated(me.Expression)) &&
+                                       me.Member is FieldInfo or PropertyInfo,
+
+                // static or pure method call
+                MethodCallExpression mc => (mc.Object == null || CanBeEvaluated(mc.Object)) &&
+                                           mc.Arguments.All(CanBeEvaluated),
+
+                InvocationExpression ie => CanBeEvaluated(ie.Expression) && ie.Arguments.All(CanBeEvaluated),
+
+                // unary / binary ops are evaluatable if all parts are
+                UnaryExpression ue => CanBeEvaluated(ue.Operand),
+                BinaryExpression be => CanBeEvaluated(be.Left) && CanBeEvaluated(be.Right),
+
                 _ => false
             };
-        }
 
         private static object? GetValue(Expression expr)
         {
@@ -42,9 +75,9 @@ internal static class ExpressionExtension
             {
                 case ConstantExpression c:
                     return c.Value;
-
-                case MemberExpression { Expression: not null } m:
-                    var target = GetValue(m.Expression);
+                
+                case MemberExpression m:
+                    var target = m.Expression != null ? GetValue(m.Expression) : null;
                     return m.Member switch
                     {
                         FieldInfo fi => fi.GetValue(target),
@@ -52,8 +85,32 @@ internal static class ExpressionExtension
                         _ => throw new NotSupportedException($"Unsupported member: {m.Member.GetType().Name}")
                     };
 
-                default:
-                    throw new NotSupportedException($"Cannot evaluate expression of type {expr.GetType().Name}");
+                case MethodCallExpression mc:
+                    var obj = mc.Object != null ? GetValue(mc.Object) : null;
+                    var args = mc.Arguments.Select(GetValue).ToArray();
+                    return mc.Method.Invoke(obj, args);
+
+                case InvocationExpression ie:
+                    // Handle calling Func<T> or delegate references
+                    var lambda = GetValue(ie.Expression);
+                    var delegateArgs = ie.Arguments.Select(GetValue).ToArray();
+                    if (lambda is Delegate del)
+                        return del.DynamicInvoke(delegateArgs);
+
+                    throw new NotSupportedException($"Cannot invoke expression of type {lambda?.GetType().Name}");
+
+                case UnaryExpression u:
+                    var operand = GetValue(u.Operand);
+                    return Expression.Lambda(u.Update(Expression.Constant(operand))).Compile().DynamicInvoke();
+
+                case BinaryExpression b:
+                    var left = GetValue(b.Left);
+                    var right = GetValue(b.Right);
+                    return Expression
+                        .Lambda(b.Update(Expression.Constant(left), b.Conversion, Expression.Constant(right))).Compile()
+                        .DynamicInvoke();
+
+                default: throw new NotSupportedException($"Cannot evaluate {expr.NodeType}");
             }
         }
     }
