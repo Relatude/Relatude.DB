8f9b4707311f438321a74b762851aba20e42b2ee
Ole Gulbrandsen <o.gulbrandsen@gmail.com>
2026-03-23 16:29:36 +0100

    Create Converter.cs

---
+++ src/Relatude.DB.DataStore/FileConverter/Converter.cs
@@ -0,0 +1,22 @@
+﻿namespace Relatude.DB.FileConverter;
+
+public class FileTypeInfo {
+}
+public enum FileInputType {
+    Image,
+    Text,
+    Audio,
+}
+public enum FileImageFormat {
+    Image,
+    Text,
+    Audio,
+}
+public enum FileOutputType {
+    Image,
+    Text,
+    Audio,
+}
+public interface Converter {
+    Task<Stream> ConvertImage(Stream input);
+}
