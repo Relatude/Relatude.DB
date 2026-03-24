c7133485286d2a019ac38394dafe98c647340ee0
Ole Gulbrandsen <o.gulbrandsen@gmail.com>
2026-03-24 08:03:53 +0100

    Update Converter.cs

--- src/Relatude.DB.DataStore/FileConverter/Converter.cs
+++ src/Relatude.DB.DataStore/FileConverter/Converter.cs
@@ -1,4 +1,6 @@
-﻿namespace Relatude.DB.FileConverter;
+﻿using Relatude.DB.Common;
+
+namespace Relatude.DB.FileConverter;
 
 public class FileTypeInfo {
 }
@@ -7,16 +9,70 @@ public enum FileInputType {
     Text,
     Audio,
 }
+public class FileMetaBase {
+    public FileValueType FileValueType { get; set; }
+}
+public abstract class FileAdjustmentBase {
+    public FileValueType FileValueType { get; set; }
+}
+public class FileAdjustmentImage : FileAdjustmentBase {
+    public int? Width { get; set; }
+    public int? Height { get; set; }
+    public int? FocusX { get; set; }
+    public int? FocusY { get; set; }
+    public double? Scale { get; set; } = null;
+    public string? Background { get; set; } = null;
+    public ImageCropMode CropMode { get; set; }
+    public FileImageFormat? Format { get; set; }
+    public int? Quality{ get; set; }
+    //public string? JsonParams { get; set; } = null;
+}
+public class FileConversionProgressInfo {
+    public FileConversionStatus Status { get; set; }
+    public int ProgressPercentage { get; set; } = 0;
+    public int RemainingSeconds { get; set; } = 0;
+    public string? Message { get; set; }
+}
+public enum ImageCropMode{ 
+    Fill,
+    Fit,
+    Auto,
+    None,
+}
+public enum FileConversionStatus {
+    Unknown,
+    Queued,
+    InProgress,
+    Done,
+    Error,
+}
+public class FileConversionResult {
+    public Guid Id { get; set; }
+    public FileConversionProgressInfo ProgressInfo { get; set; }
+    public Stream? Output { get; set; }
+}
+public class ImageMeta{
+    public string? Photographer { get; set; }
+    public string? Copyright { get; set; }
+    public double? GPSLatitude { get; set; }
+    public double? GPSLongitude { get; set; }
+    public string? AutoDescription { get; set; }
+    public string? AutoKeywords { get; set; }
+}
 public enum FileImageFormat {
-    Image,
-    Text,
-    Audio,
+    Jpeg,
+    Png,
+    Webp,
+    Gif,
+    Bmp,
 }
 public enum FileOutputType {
-    Image,
-    Text,
-    Audio,
 }
-public interface Converter {
-    Task<Stream> ConvertImage(Stream input);
+public interface Converter { // just the convertion
+    Task<ImageMeta?> AnalyzeImageAsync(Stream input, FileValueType inputType);
+    Task<FileConversionResult?> ConvertAsync(Stream input, FileValueType inputType, FileAdjustmentBase adjustments);
+    Task<FileConversionProgressInfo> GetStatusAsync(Guid id);
+    Task<Stream> GetStreamAsync(Stream input, string inputHash, FileValueType inputType, FileAdjustmentBase adjustments);
+}
+public interface ConvertServer{ // includes storage and cached previous results
 }
