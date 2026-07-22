2c8fa41156c2778fe6da46c78374d01a4e156c49
Ole Gulbrandsen <o.gulbrandsen@gmail.com>
2026-07-22 08:16:47 +0200

    dev

--- examples/Website.Simple/CMSModels/CMSRelations.cs
+++
@@ -1,9 +0,0 @@
-﻿using Relatude.DB.Nodes;
-
-namespace Relatude.CMS.Models {
-    public class TemplatePages : OneToMany<ITemplate, IPage> {
-        public class Template : One { }
-        public class Pages : Many { }
-    }
-}
-
--- examples/Website.Simple/CMSModels/ICountry.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Relatude.CMS {
-    public interface ICountry {
-        public string TwoLetterCode { get; set; }
-        public string ThreeLetterCode { get; set; }
-
-    }
-}
