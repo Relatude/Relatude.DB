04e0effc89e92431ea5ffacd8483169b8076d043
Ole Gulbrandsen <o.gulbrandsen@gmail.com>
2026-09-17 23:15:51 +0200

    minor

--- benchmarks/Benchmark.Site/Program.cs
+++ benchmarks/Benchmark.Site/Program.cs
@@ -13,7 +13,7 @@ app.UseStaticFiles();
 
 app.MapPost("/start", () => {
     Status.Current.Running = true;
-    var dataSetMultiplier = 500000;
+    var dataSetMultiplier = 50000;
     var timeMultiplier = 2;
     var options = new TestOptions();
     options.FlushDiskOnEveryOperation = false;
@@ -27,11 +27,11 @@ app.MapPost("/start", () => {
     ITester[] testers = [
 
         //new MsSqlDBTester(),
-        // new RavenDBEmbeddedTester(),
+        new RavenDBEmbeddedTester(),
         //new LiteDBTester(),
-        // new SQLiteDBTester(),
-        //new RelatudeDBTester( RelatudeDiskFlushMode.StreamFlush, RelatudeIndexType.Sqlite),
-        //new RelatudeDBTester( RelatudeDiskFlushMode.StreamFlush, RelatudeIndexType.Native),
+        new SQLiteDBTester(),
+        //new RelatudeDBTester( RelatudeDiskFlushMode.AutoFlush, RelatudeIndexType.Sqlite),
+        //new RelatudeDBTester( RelatudeDiskFlushMode.AutoFlush, RelatudeIndexType.Native),
         new RelatudeDBTester( RelatudeDiskFlushMode.AutoFlush, RelatudeIndexType.Native),
         new RelatudeDBTester( RelatudeDiskFlushMode.AutoFlush, RelatudeIndexType.Memory),
         new RelatudeDBTester( RelatudeDiskFlushMode.NoFlush, RelatudeIndexType.Native),
